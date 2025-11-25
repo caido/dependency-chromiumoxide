5191dc075f9f538d7105f3c48a005b6e8916f443
Emile Fugulin <code@efugulin.com>
2025-11-25 00:41:37 -0500

    Allow deprecated use in CDP

--- chromiumoxide_cdp/src/lib.rs
+++ chromiumoxide_cdp/src/lib.rs
@@ -11,6 +11,7 @@ use crate::revision::Revision;
 #[allow(clippy::multiple_bound_locations)]
 #[allow(clippy::derive_partial_eq_without_eq)]
 #[allow(unreachable_patterns)]
+#[allow(deprecated)]
 pub mod cdp;
 pub mod revision;
 
